627265a3b6998af64a04a88d488a4d52dda71b5e
Martin Michelsen <mjem@wildblue.net>
2017-07-08 13:29:11 -0700

    add some more tests

--- run_tests.sh
+++ run_tests.sh
@@ -8,5 +8,7 @@ echo "-- tests.hello_world_combination"
 ./nemesys tests.hello_world_combination | diff - tests/hello_world.expected_output
 echo "-- tests.echo_once"
 echo 'Hello world!' | ./nemesys tests.echo_once | diff - tests/hello_world.expected_output
+echo "-- tests.repr"
+./nemesys tests.repr | diff - tests/repr.expected_output
 
 echo "-- all tests passed"
---
+++ tests/repr.py
@@ -0,0 +1,12 @@
+print("repr tests...")
+print("repr(None) -> " + repr(None))
+print("repr(True) -> " + repr(True))
+print("repr(not True) -> " + repr(not True))
+print("repr(5) -> " + repr(5))
+print("repr(5 + 11) -> " + repr(5 + 11))
+# print("repr(6.0) -> " + repr(6.0))  # no floats yet
+# print("repr(6.0 + 3) -> " + repr(6.0 + 3))  # no floats yet
+print("repr(b'omg') -> " + repr(b'omg'))
+print("repr(b'omg' + b'hax') -> " + repr(b'omg' + b'hax'))
+print("repr('omg') -> " + repr('omg'))
+print("repr('omg' + 'hax') -> " + repr('omg' + 'hax'))
