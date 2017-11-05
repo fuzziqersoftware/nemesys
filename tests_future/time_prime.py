31dac9afcc4cdcf7e85a7f02fcbdc52159cdbcad
Martin Michelsen <mjem@wildblue.net>
2017-11-04 22:33:09 -0700

    add prime test
    - nemesys currently runs this about 9.5x faster than python3 on my laptop

---
+++ tests_future/time_prime.py
@@ -0,0 +1,19 @@
+def is_prime(a):
+  i = 2
+  limit = a // 2
+  while i <= limit:
+    if a % i == 0:
+      return False
+    i = i + 1
+  return True
+
+
+a = 2
+while a < 100000:
+  if is_prime(a):
+    print('%d is prime' % a)
+  else:
+    print('%d is not prime' % a)
+  a = a + 1
+
+print('done')
