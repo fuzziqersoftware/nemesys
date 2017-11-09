2737dbb2d1c1bf8f41f297be77ef7ade56706dc8
Martin Michelsen <mjem@wildblue.net>
2017-11-09 00:08:16 -0800

    add hash note

---
+++ notes/hash.s
@@ -0,0 +1,22 @@
+.intel_syntax noprefix
+
+.globl fnv1a64
+
+_fnv1a64_start:
+  mov   rdx, 0xCBF29CE484222325
+
+_fnv1a64_continue:
+  add   rsi, rdi # rsi = end ptr
+  xor   rax, rax
+  mov   rcx, 0x00000100000001B3
+
+_fnv1a64_continue__again:
+  mov   al, [rdi]
+  xor   rdx, rax
+  imul  rdx, rcx
+  inc   rdi
+  cmp   rdi, rsi
+  jne   _fnv1a64_continue__again
+
+  mov   rax, rdx
+  ret
